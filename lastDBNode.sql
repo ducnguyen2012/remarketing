c9dc223125447088e2946e480e44e9f14824d63e
ducnguyen2012 <166124443+ducnguyen2012@users.noreply.github.com>
2025-06-30 15:48:32 +0700

    Update lastDBNode.sql

--- lastDBNode.sql
+++ lastDBNode.sql
@@ -1,5 +1,5 @@
 INSERT into information(
   created_at,remarketing_message,conversation_id,last_seen_user_message_created_at
 ) values (
-  '{{ $json.created_at }}','{{ $json.rmktMessage}}','{{ $json.conversation_id }}', '{{ $json.created_at }}'
+  '{{ $json.currentTime }}','{{ $json.rmktMessage}}','{{ $json.conversation_id }}', '{{ $json.created_at }}'
 )
